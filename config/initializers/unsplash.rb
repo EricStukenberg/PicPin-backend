cd2d1b913614657581f5649f8a4ffa78f74ad407
stukene <ericstukenberg@outlook.com>
2020-06-18 17:10:28 -0700

    added Unspash Init

--- config/initializers/unsplash.rb
+++ config/initializers/unsplash.rb
@@ -1,6 +1,6 @@
 Unsplash.configure do |config|
-    config.application_access_key = "YOUR ACCESS KEY"
-    config.application_secret = "YOUR APPLICATION SECRET"
+    config.application_access_key = "IxILOxNvWo6cib8pxWCzhypHFykKpKGB27s7oRFkYBo"
+    config.application_secret = "fmqWNJ29RJ_V5N9p_Zrm1Q_HJ9WQNVW6iDeVDx3TSSw"
     config.application_redirect_uri = "https://your-application.com/oauth/callback"
     config.utm_source = "alices_terrific_client_app"
   
